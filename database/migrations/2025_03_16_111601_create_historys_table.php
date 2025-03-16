17bc82d7369c2c60ccbd913d3198c79d20c4aae2
daiki <kishita167@icloud.com>
2025-03-16 23:56:17 +0900

    データの型変更

--- database/migrations/2025_03_16_111601_create_historys_table.php
+++ database/migrations/2025_03_16_111601_create_historys_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('historys', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('purpose');
             $table->string('type_id');
             $table->integer('value');
