bacc28bbf566b69c30366a3e6cc1ca6746b60878
daiki <kishita167@icloud.com>
2025-03-21 22:32:47 +0900

    sample用routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\SampleController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('sample',SampleController::class.'@index');
