bbca0862bf0197edf60b70ac0bada2cb1ea09c09
daiki <kishita167@icloud.com>
2025-03-22 20:55:53 +0900

    headerとfooterの書式変更

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,3 @@
-<div class="footer">
-    copyright 2025 T2an
-</div>
+<footer class="bg-dark text-white text-center py-3 mt-5 fixed-bottom">
+    copyright 2025 T2an 
+</footer>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,3 @@
-<div class="header">
+<header class = "bg-dark text-white text-center py-3">
     <h1>家計簿アプリ DC</h1>
-</div>
+</header>
