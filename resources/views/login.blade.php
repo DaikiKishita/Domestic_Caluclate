d251a6394a7c0bc52325188c6d8640f569fc4f77
daiki <kishita167@icloud.com>
2025-03-22 21:45:54 +0900

    loginpage

---
+++ resources/views/login.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.layout')
+@section('content')
+<div class = "d-flex flex-column justify-content-center align-items-center bg-dark-subtle py-4">
+    <h4>ログイン</h4>
+    <form method="POST" action="/">
+        @csrf
+        <div class="mb-3">
+            <label for="email" class="form-label">メールアドレス</label>
+            <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" required autofocus>
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">パスワード</label>
+            <input type="password" class="form-control" id="password" name="password" required>
+        </div>
+        <!-- <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="remember" name="remember">
+            <label class="form-check-label" for="remember">ログイン状態を保存する</label>
+        </div> -->
+        <div class="text-center">
+            <button type="submit" class="btn btn-primary">ログイン</button>
+        </div>
+
+        <div class = "text-center mt-3">
+            <a href="/register">新規登録はこちら</a>
+        </div>
+    </form>
+</div>
+@endsection
