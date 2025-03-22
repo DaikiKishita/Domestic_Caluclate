01eed169171ee976ea8a4590accbb158e20743b3
daiki <kishita167@icloud.com>
2025-03-22 21:42:23 +0900

    header,footerへんこう

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,3 @@
-<footer class="bg-dark text-white text-center py-3 mt-5 fixed-bottom">
+<footer class="bg-dark text-white text-center py-1 mt-5 fixed-bottom">
     copyright 2025 T2an 
 </footer>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,3 @@
-<header class = "bg-dark text-white text-center py-3">
+<header class = "bg-dark text-white text-center py-1">
     <h1>家計簿アプリ DC</h1>
 </header>
