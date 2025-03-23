e138e9ad163b49e43d66edfd5041e5385b34783a
daiki <kishita167@icloud.com>
2025-03-23 10:13:19 +0900

    historys -> histories

---
+++ database/migrations/2025_03_16_111601_create_histories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('historys', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('purpose');
+            $table->foreignId('type_id');
+            $table->integer('value');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('historys');
+    }
+};
