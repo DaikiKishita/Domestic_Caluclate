7a0e72c8bc37eef16bf2d3ee16b68274463cf268
daiki <kishita167@icloud.com>
2025-03-23 10:12:53 +0900

    historys -> histories

---
+++ app/Models/History.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Type;
+
+class History extends Model
+{
+    //
+    protected $fillable = [
+        'purpose',
+        'value',
+        'type_id',
+    ]
+
+    public function type(){
+        return $this->belongsTo(Type::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'purpose' => 'string',
+            'value' => 'integer',
+        ];
+    }
+}
