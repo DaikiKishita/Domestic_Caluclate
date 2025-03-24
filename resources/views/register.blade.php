bab25c1c3cdd2048ac98423ac315fc34de00cd4e
daiki <kishita167@icloud.com>
2025-03-24 20:18:32 +0900

    新規登録ページのパスワード確認用プログラムの追加

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.layout')
 @section('content')
-<div class="d-flex flex-column justify-content-center align-items-center bg-dark-subtle py-4">
+<div class="d-flex flex-column justify-content-center align-items-center bg-dark-subtle py-2">
     <h4>新規登録</h4>
-    <form method="POST" action="/user/new">
+    <form method="POST" action="/user/create">
         @csrf
         <div class="mb-3">
             <label for="name" class="form-label">ユーザー名</label>
@@ -17,12 +17,35 @@
             <input type="password" class="form-control" id="password" name="password" required>
         </div>
         <div class="mb-3">
+            <p id = "warning_message" class = "text-danger"></p>
             <label for="password_confirmation" class="form-label">パスワード（確認）</label>
             <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
         </div>
         <div class="text-center">
-            <button type="submit" class="btn btn-primary">新規登録</button>
+            <button type="submit" class="btn btn-primary" id = "submit_button">新規登録</button>
         </div>
     </form>
 </div>
+<script>
+    let confirmation_password = document.getElementById('password_confirmation');
+    let warning_message = document.getElementById('warning_message');
+
+    // 新規登録ボタンを無効にする
+    let submit_button = document.getElementById('submit_button');
+    submit_button.disabled = true;
+
+    // パスワードと確認用パスワードが一致しているか確認
+    confirmation_password.addEventListener('change', function() {
+
+        let password = document.getElementById('password').value;
+        let password_confirmation = confirmation_password.value;
+
+        if (password !== password_confirmation) {
+            warning_message.innerText = 'パスワードが一致しません';
+        } else {
+            warning_message.innerText = '';
+            submit_button.disabled = false;
+        }
+    });
+</script>
 @endsection
