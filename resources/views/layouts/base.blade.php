78630889044964a5bbb0e02efa47d5a0dd9f740d
daiki <kishita167@icloud.com>
2025-03-26 00:45:05 +0900

    背景色指定

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,21 +6,8 @@
     <title>家計簿アプリ DC</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <style>
-    body {
-        background: repeating-linear-gradient(
-            45deg,          /* 斜めのストライプ */
-            #f8f9fa,        /* 薄いグレー */
-            #f8f9fa 10px,   /* ストライプの幅 */
-            #dee2e6 10px,   /* 濃いグレー */
-            #dee2e6 20px    /* ストライプの間隔 */
-        );
-        height: 100vh; /* 画面全体をカバー */
-        margin: 0;
-    }
-</style>
 </head>
-<body>
+<body class='bg-light'>
     @yield('layout')
 </body>
 </html>
