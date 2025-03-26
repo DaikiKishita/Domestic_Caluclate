d95747a72f223a15c0560047b5dc9b04ccc968e4
daiki <kishita167@icloud.com>
2025-03-26 15:43:07 +0900

    表示修正

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.base')
 @section('layout')
-    @include('components.header', ['is_login' => $is_login])
+    @if (isset($is_login))
+        @include('components.header', ['is_login' => $is_login])
+    @else
+        @include('components.header')
+    @endif
 
     <!-- コンテンツ部分 -->
     @yield('content')
